ad82e1c8eba79e8373c9857f8b0ee8227a81b435
Tyoma <risendrums777@gmail.com>
2025-04-24 15:20:35 +0300

    Update ResponsibleGambling.tsx

--- src/components/ResponsibleGambling.tsx
+++ src/components/ResponsibleGambling.tsx
@@ -8,23 +8,23 @@ const ResponsibleGambling = () => {
         <h3 className="text-2xl font-bold text-center text-white mb-8">Play Safely & Responsibly</h3>
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-6 items-center justify-items-center">
           <a href="https://www.gambleaware.org/home/" target="_blank" rel="noopener noreferrer">
-            <img src="https://slotdomempire.eu/lovable-uploads/f667e2a6-579c-4a35-b893-aae13512a4b2.png" alt="GambleAware" className="h-12 w-auto" />
+            <img src="/images/compliance/gambleaware.png" alt="GambleAware" className="h-14 w-auto" />
           </a>
-          <img src="https://slotdomempire.eu/lovable-uploads/88b69db6-2153-4091-abda-30745b9db5bc.png" alt="18+" className="h-12 w-auto" />
+          <img src="/images/compliance/18.png" alt="18+" className="h-12 w-auto" />
           <a href="https://www.gamblingtherapy.org/" target="_blank" rel="noopener noreferrer">
-            <img src="https://slotdomempire.eu/lovable-uploads/03e9b0d0-43f1-4277-a351-306561d7a8bf.png" alt="Gordon Moody" className="h-12 w-auto" />
+            <img src="/images/compliance/gordon-moody.png" alt="Gordon Moody" className="h-14 w-auto" />
           </a>
           <a href="https://www.gbga.gi/" target="_blank" rel="noopener noreferrer">
-            <img src="https://slotdomempire.eu/lovable-uploads/af498e70-45cb-4e16-ac2b-bf6b3bb0967f.png" alt="GBGA" className="h-12 w-auto" />
+            <img src="/images/compliance/GBGA.png" alt="GBGA" className="h-14 w-auto" />
           </a>
           <a href="https://ibas-uk.com/" target="_blank" rel="noopener noreferrer">
-            <img src="https://slotdomempire.eu/lovable-uploads/4ed3acbe-58bc-4af5-b4ce-a20383797a75.png" alt="IBAS" className="h-12 w-auto" />
+            <img src="/images/compliance/ibas.png" alt="IBAS" className="h-14 w-auto" />
           </a>
           <a href="https://www.gamcare.org.uk/" target="_blank" rel="noopener noreferrer">
-            <img src="https://slotdomempire.eu/lovable-uploads/3d8b70d1-4c47-4fdd-9d1d-58e34620ef74.png" alt="GamCare" className="h-12 w-auto" />
+            <img src="/images/compliance/gamcare.png" alt="GamCare" className="h-14 w-auto" />
           </a>
           <a href="https://www.gamblingcommission.gov.uk/" target="_blank" rel="noopener noreferrer">
-            <img src="https://slotdomempire.eu/lovable-uploads/b733b72a-5d80-40c0-aa7f-d611dcff9927.png" alt="Gambling Commission" className="h-12 w-auto" />
+            <img src="/images/compliance/gambling-commission.png" alt="Gambling Commission" className="h-14 w-auto" />
           </a>
         </div>
       </div>
