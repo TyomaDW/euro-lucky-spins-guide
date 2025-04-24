0b474ece6afba78fad6787ed116102857d7d6a94
Tyoma <risendrums777@gmail.com>
2025-04-24 15:20:55 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,16 @@ import Index from "./pages/Index";
 import About from "./pages/About";
 import Faq from "./pages/Faq";
 import Contact from "./pages/Contact";
+import Privacy from "./pages/Privacy";
+import CookiePolicy from "./pages/CookiePolicy";
+import Disclosure from "./pages/Disclosure";
+import Terms from "./pages/Terms";
+import Casinogames from "./pages/Casinogames";
+import Roulette from "./pages/Roulette";
+import Slots from "./pages/Slots";
+import Livecasino from "./pages/Livecasino";
+import RankingMethodology from "./pages/Rankingmethodology";
+import News from "./pages/News";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -23,6 +33,17 @@ const App = () => (
           <Route path="/about" element={<About />} />
           <Route path="/faq" element={<Faq />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/Privacy" element={<Privacy />} />
+          <Route path="/CookiePolicy" element={<CookiePolicy />} />
+          <Route path="/Disclosure" element={<Disclosure />} />
+          <Route path="/Terms" element={<Terms />} />
+          <Route path="/casino-games" element={<Casinogames />} />
+          <Route path="/roulette" element={<Roulette />} />
+          <Route path="/slots" element={<Slots />} />
+          <Route path="/live-casino" element={<Livecasino />} />
+          <Route path="/ranking-methodology" element={<RankingMethodology />} />
+          <Route path="/news" element={<News />} />
+
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
